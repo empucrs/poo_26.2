47f0db79f55da17acd5f7da421d9b3d83368c38a
Edson Moreno <51421368+empucrs@users.noreply.github.com>
2026-08-05 12:27:39 +0000

    Criação do projeto inicial

---
+++ Aula0805-Teste/src/main/java/br/pucrs/App.java
@@ -0,0 +1,7 @@
+package br.pucrs;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto Aula0805-Teste criado com sucesso.");
+    }
+}
